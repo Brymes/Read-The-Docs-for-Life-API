d3f716b1022b02aa3be00f6d9f7ca7c2e3cde29d
Timothy Odebunmi <dev.brymes@gmail.com>
2021-09-15 11:34:39 +0100

    Wrote Fields for Experiences

--- ent/schema/experience.go
+++ ent/schema/experience.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+	"time"
+)
 
 // Experience holds the schema definition for the Experience entity.
 type Experience struct {
@@ -9,7 +14,13 @@ type Experience struct {
 
 // Fields of the Experience.
 func (Experience) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("uuid", uuid.UUID{}).Default(uuid.New),
+		field.Time("created_at").Default(time.Now),
+		field.String("Title").NotEmpty().MaxLen(75),
+		field.Text("Content").Unique().NotEmpty(),
+		field.Int("Views").Positive(),
+	}
 }
 
 // Edges of the Experience.
